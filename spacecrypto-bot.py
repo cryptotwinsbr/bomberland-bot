3ebfdc52f4d6f16007c796d6e739ff18010d2edf
cryptotwinsbr <cryptotwinsbr@gmail.com>
2022-02-15 11:25:38 -0300

    add login, retry, into

--- spacecrypto-bot.py
+++ spacecrypto-bot.py
@@ -10,11 +10,36 @@ import time
 import sys
 
 # Tempo entre ações
-pyautogui.PAUSE = 1
+pyautogui.PAUSE = 0.5
 global x_scroll
 global y_scroll
 global h_scroll
 global w_scroll
+
+str_in = """
+            ░░░░░░░█▐▓▓░████▄▄▄█▀▄▓▓▓▌█
+            ░░░░░▄█▌▀▄▓▓▄▄▄▄▀▀▀▄▓▓▓▓▓▌█
+            ░░░▄█▀▀▄▓█▓▓▓▓▓▓▓▓▓▓▓▓▀░▓▌█
+            ░░█▀▄▓▓▓███▓▓▓███▓▓▓▄░░▄▓▐█▌
+            ░█▌▓▓▓▀▀▓▓▓▓███▓▓▓▓▓▓▓▄▀▓▓▐█
+            ▐█▐██▐░▄▓▓▓▓▓▀▄░▀▓▓▓▓▓▓▓▓▓▌█▌
+            █▌███▓▓▓▓▓▓▓▓▐░░▄▓▓███▓▓▓▄▀▐█
+            █▐█▓▀░░▀▓▓▓▓▓▓▓▓▓██████▓▓▓▓▐█
+            ▌▓▄▌▀░▀░▐▀█▄▓▓██████████▓▓▓▌█▌
+            ▌▓▓▓▄▄▀▀▓▓▓▀▓▓▓▓▓▓▓▓█▓█▓█▓▓▌█▌
+            █▐▓▓▓▓▓▓▄▄▄▓▓▓▓▓▓█▓█▓█▓█▓▓▓▐█ 
+    +++++++++++++++++++++++++++++++++++++++++++++++
+    +++ 🌙 Melhorando nossas noites de sono 🌙 +++
+    +++ Se te ajudamos, por favor contribua 😊🚀++
+    ++++++++++++ SPG SPE BCOIN BUSD BNB +++++++++++
+    + 0x73933b679F940ea7352c3895852501e3044FE855 ++
+    ++++++++++++++++ Pix key ++++++++++++++++++++++
+    ++++ 5f3d220c-a2a3-4db2-bfb2-30ae0533e240 +++++
+
+    >> Ctrl + c finaliza o bot.
+    
+    
+    """
 
 def addRandomness(n, randomn_factor_size=None):
     if randomn_factor_size is None:
@@ -91,66 +116,13 @@ def positions(target, threshold=0.8,img = None):
     rectangles, weights = cv2.groupRectangles(rectangles, 1, 0.2)
     return rectangles
 
-def click_fight_ship():
-    global x_scroll
-    global y_scroll
-    global h_scroll
-    global w_scroll
-
-    offset_x = 220
-    offset_y = 50
-    y_ship_final = 0
-
-    green_bars = positions(images['blue-bar-short'], threshold=0.9)
-    print('Blue bars detected', len(green_bars))
-    buttons = positions(images['fight'], threshold=0.9)
-    print('Buttons fight detected', len(buttons))
-
-    for key,(x, y, w, h) in enumerate(buttons):
-        print('key: ', key)
-        if key == 0:
-            x_scroll = x
-            y_scroll = y
-            h_scroll = h
-            w_scroll = w
-        elif key == 2:
-            y_ship_final = y
-            print("Y ship final: ", y_ship_final)        
-
-    yellow_bars = positions(images['yellow-bar-short'], threshold=0.9)
-    print('Yellow bars detected', len(yellow_bars))
-
-    not_working_green_bars = []
-    for bar in green_bars:
-        not_working_green_bars.append(bar)
-    for bar in yellow_bars:
-        not_working_green_bars.append(bar)
-    if len(not_working_green_bars) > 0:
-        print('buttons with green bar detected', len(not_working_green_bars))
-        print('Clicking in heroes', len(not_working_green_bars))
-    hero_clicks_cnt = 0
-    for (x, y, w, h) in not_working_green_bars:
-        print("Entrou for x y w h. Y:", y)
-        if ( y < y_ship_final+50):
-            moveToWithRandomness(x+offset_x+(w/2),y+offset_y+(h/2),1)
-            pyautogui.click()
-            global hero_clicks
-            hero_clicks = hero_clicks + 1
-            hero_clicks_cnt = hero_clicks_cnt + 1
-            if hero_clicks_cnt > 20:
-                print('Too many hero clicks, try to increase the go_to_work_btn threshold')
-                return
-        else:
-            print("Botao abaixo")
-    return len(not_working_green_bars)
-
 def scroll_ships():
     global x_scroll
     global y_scroll
     global h_scroll
     global w_scroll
     use_click_and_drag_instead_of_scroll = True
-    click_and_drag_amount = 200
+    click_and_drag_amount = 80
     scroll_size = 60
 
     moveToWithRandomness(x_scroll+(w_scroll/2),y_scroll+300+(h_scroll/2),1)
@@ -161,21 +133,22 @@ def scroll_ships():
 
 def go_to_continue():
     if clickBtn(images['confirm']):
-        print('confirm clicked')
+        print('Encontrou confirm')
         return True
     else:
         return False
 
 def go_to_ship():
     if clickBtn(images['ship']):
-        print('ship clicked')
+        print('Encontrou ship buttom')
         return True
     else:
         return False
 
 def go_to_fight():
     if clickBtn(images['fight-boss-new']):
-        print('fight-boss clicked')
+        print('''Vai para fight boss!!
+        ''')
 
 def ships_15_15():
     start = time.time()
@@ -186,7 +159,7 @@ def ships_15_15():
         if(len(matches)==0):
             has_timed_out = time.time()-start > 3
             continue
-        print('Encontrou 15-15')
+        print('Encontrou 15-15 tela naves')
         return True
     return False
 
@@ -216,18 +189,79 @@ def time_is_zero():
         return True
     print('Time diferente de zero')
     return False
+
+def click_fight_ship_new():
+    global x_scroll
+    global y_scroll
+    global h_scroll
+    global w_scroll
+
+    offset_x = 220
+    offset_y = 50
+    y_ship_final = 0
+
+    green_bars = positions(images['blue-bar-short'], threshold=0.9)
+    #print('Blue bars detected', len(green_bars))
+    buttons = positions(images['fight'], threshold=0.9)
+    #print('Buttons fight detected', len(buttons))
+
+    for key,(x, y, w, h) in enumerate(buttons):
+        #print('key: ', key)
+        if key == 0:
+            x_scroll = x
+            y_scroll = y
+            h_scroll = h
+            w_scroll = w
+        elif key == 2:
+            y_ship_final = y
+            #print("Y ship final: ", y_ship_final)        
+
+    yellow_bars = positions(images['yellow-bar-short'], threshold=0.9)
+    #print('Yellow bars detected', len(yellow_bars))
+
+    not_working_green_bars = []
+    for bar in green_bars:
+        not_working_green_bars.append(bar)
+    for bar in yellow_bars:
+        not_working_green_bars.append(bar)
+    #if len(not_working_green_bars) > 0:
+        #print('buttons with green bar detected', len(not_working_green_bars))
+        #print('Naves disponiveis', len(not_working_green_bars))
+    ship_clicks_cnt = 0
+    for (x, y, w, h) in not_working_green_bars:
+        #print("Entrou for x y w h. Y:", y)
+        if ( y < y_ship_final+50):
+            moveToWithRandomness(x+offset_x+(w/2),y+offset_y+(h/2),1)
+            for i in range(len(not_working_green_bars)):
+                pyautogui.click()
+                global ship_clicks
+                ship_clicks = ship_clicks + 1
+                ship_clicks_cnt = ship_clicks_cnt + 1
+                if ship_clicks > 15:
+                    return            
+            print("Qtd ship enviadas: " + str(ship_clicks_cnt) + ". " + "Qtd ship total enviadas: " + str(ship_clicks))   
+            #print("Qtd ship total enviadas", ship_clicks) 
+            return
+        else:
+            print("Botao abaixo")      
+    return len(not_working_green_bars)
+
        
-def ship_to_fight():
+def ship_to_fight():    
+    global ship_clicks
     #if time_is_zero():
     if go_to_ship():
+        ship_clicks = 0
         buttonsClicked = 1
         empty_scrolls_attempts = 3
         while(empty_scrolls_attempts >0):
-            buttonsClicked = click_fight_ship()
+            buttonsClicked = click_fight_ship_new()
             if buttonsClicked == 0:
                 empty_scrolls_attempts = empty_scrolls_attempts - 1
             if ships_15_15():
-                break
+                break            
+            if ship_clicks > 15:
+                break    
             scroll_ships()
             time.sleep(1)
         go_to_fight()
@@ -238,7 +272,7 @@ def ship_to_fight():
 
 def go_to_ship_tela_boss():
     if clickBtn(images['ship-boss']):
-        print('ship tela boss clicked')
+        print('Volta para naves, tela boss')
         return True
     else:   
         return False
@@ -253,7 +287,7 @@ def ship_tela_boss():
             empty_scrolls_attempts = 3
 
             while(empty_scrolls_attempts >0):
-                buttonsClicked = click_fight_ship()
+                buttonsClicked = click_fight_ship_new()
                 if buttonsClicked == 0:
                     empty_scrolls_attempts = empty_scrolls_attempts - 1
                 if ships_15_15():
@@ -262,20 +296,49 @@ def ship_tela_boss():
                 time.sleep(2)
             go_to_fight()
 
+def login():
+    global login_attempts
+    print("Verificando se o jogo foi desconectado")
+
+    if login_attempts > 3:
+        print('Tres tentativas de login, dando refresh na pagina')
+        login_attempts = 0
+        pyautogui.hotkey('ctrl','f5')
+        return
+
+    if clickBtn(images['connect-wallet'], timeout = 10):
+        print('Connect wallet encontrado')
+        login_attempts = login_attempts + 1
+
+    if clickBtn(images['sign'], timeout=8):
+        login_attempts = login_attempts + 1
+        print('Sign button encontrado')
+        
+        if clickBtn(images['play'], timeout = 15):
+            print('Botao play encontrado')
+            print('''Jogo iniciado com sucesso!!
+
+            ''')
+            login_attempts = 0
+        return
+
 def main():
     global images    
-    global hero_clicks
-    hero_clicks = 0
+    global ship_clicks
+    global login_attempts
+    ship_clicks = 0
+    login_attempts = 0
     images = load_images()
+
+    print(str_in)    
+    time.sleep(5)
     
-    #print("""Opa beleza?""")
     '''print('Quantas naves(minimo) você deseja para iniciar a batalha?')
     qtd_ships = input()
     print('Quantidade de naves selecionadas: ' + qtd_ships)'''
 
-    time.sleep(1)
-
     time_start = {
+    "login" : 0,
     "ship_to_fight" : 0,
     "ship" : 0,
     "fight" : 0,
@@ -285,32 +348,39 @@ def main():
     }
 
     time_to_check = {
+    "login" : 1,
     "ship_to_fight" : 5,
     "ship" : 10,
     "fight" : 5,
     "fight_boss" : 20,
     "ship_tela_boss": 3,
-    "continue": 5,
+    "continue": 1,
     }
+
     while True:
         actual_time = time.time()
 
         if actual_time - time_start["ship_to_fight"] > time_to_check['ship_to_fight']:
                 time_start["ship_to_fight"] = actual_time
-                print("Ship to fight")
+                #print("Ship to fight")
                 ship_to_fight()
         
         if actual_time - time_start["ship_tela_boss"] > time_to_check['ship_tela_boss']:
                 time_start["ship_tela_boss"] = actual_time
-                print("Ship tela boss")
+                #print("Ship tela boss")
                 ship_tela_boss()
 
         if actual_time - time_start["continue"] > time_to_check['continue']:
                 time_start["continue"] = actual_time
-                print("Ship continue")
-                go_to_continue()
+                #print("Ship continue")
+                go_to_continue()        
 
-        time.sleep(1)
+        if actual_time - time_start["login"] > addRandomness(time_to_check['login'] * 60):
+            sys.stdout.flush()
+            time_start["login"] = actual_time
+            login()
+
+        time.sleep(0.3)
 
 if __name__ == '__main__':
     main()
